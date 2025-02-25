ca31c20f8d288ad71a126aa93b2ddfb8310116a1
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-02-26 00:22:12 +0100

    animation on camera working >w<

--- Assets/Emma's folder x3/Camera_controller.cs
+++ Assets/Emma's folder x3/Camera_controller.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,12 @@ public class Camera_controller : MonoBehaviour
     public float sensX;
     public float sensY;
 
+    public float aniX;
+    public float aniY;
+    public float aniZ;
+
     public Transform Orientation;
+    public GameObject Animator;
 
     public float xRotation;
     public float yRotation;
@@ -18,12 +24,21 @@ public class Camera_controller : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        
+
+        Animator = GameObject.FindGameObjectWithTag("Cam_animation");
+        aniX = Animator.transform.rotation.eulerAngles.x;
+        aniY = Animator.transform.rotation.eulerAngles.y;
+        aniZ = Animator.transform.rotation.eulerAngles.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        aniX = Animator.transform.rotation.eulerAngles.x;
+        aniY = Animator.transform.rotation.eulerAngles.y;
+        aniZ = Animator.transform.rotation.eulerAngles.z;
+
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
@@ -32,7 +47,7 @@ public class Camera_controller : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        transform.rotation = Quaternion.Euler(xRotation + aniX, yRotation + aniY, aniZ);
+        Orientation.rotation = Quaternion.Euler(0, yRotation + aniY, aniY);
     }
 }
