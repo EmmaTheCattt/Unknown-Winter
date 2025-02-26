992c8dac42daf3327d4b1b438a186d88d0a194f2
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-02-26 03:27:11 +0100

    animations

--- Assets/Emma's folder x3/Movement.cs
+++ Assets/Emma's folder x3/Movement.cs
@@ -9,12 +9,14 @@ public class Movement : MonoBehaviour
 
     [Header("Movement")]
     public float movespeed = 1f;
+    public bool walking = false;
+    public float GroundDrag;
+
+    [Header("Broken leg")]
+    //broken leg.
     public bool leg_broken = true;
     public float Leg_push = 0f;
     public float Leg_push_time = 0.1f;
-
-    public float GroundDrag;
-
     public float move_wait_time = 1f;
 
     [Header("Ground Check")]
@@ -22,9 +24,13 @@ public class Movement : MonoBehaviour
     public LayerMask Ground;
     public bool grounded;
 
+
+    [Header("Other")]
     public Rigidbody rb;
 
     public Transform Orientation;
+    public GameObject Cam_animation;
+    public Animator Animator;
 
     public float horizontalInput;
     public float verticalInput;
@@ -37,8 +43,8 @@ public class Movement : MonoBehaviour
         rb.freezeRotation = true;
 
         Orientation = GameObject.FindGameObjectWithTag("Orientation").transform;
-
-
+        Cam_animation = GameObject.FindGameObjectWithTag("Cam_animation");
+        Animator = Cam_animation.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -54,9 +60,12 @@ public class Movement : MonoBehaviour
         {
             rb.drag = 0;
         }
-
-        MyInput();
         
+        MyInput();
+
+
+        Animator.SetBool("Walk", walking);
+
     }
 
     private void FixedUpdate()
@@ -68,6 +77,15 @@ public class Movement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            walking = true;
+        }
+        else
+        {
+            walking = false;
+        }
     }
 
     void MovePlayer()
