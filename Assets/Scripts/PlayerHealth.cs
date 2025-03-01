c47142d0a323f2c68fb7e850801412044b2ca9a9
eakru2003 <eakruse1234@gmail.com>
2025-03-01 23:40:20 +0100

    Healthbar

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float StartingHealth = 100f;
+    public Slider slider;
+    public Image FillImage;
+    public Color FullHealthColor = Color.green;
+    public Color ZeroHealthColor = Color.red;
+    private float CurrentHealth;
+    private bool Dead;
+
+    private void OnEnable()
+    {
+        CurrentHealth = StartingHealth;
+        Dead = false;
+        SetHealthUI();
+    }
+    public void TakeDamage(float damage) 
+    {
+    CurrentHealth -= damage;
+        SetHealthUI();
+        if (CurrentHealth <= 0 && !Dead)
+        { 
+        OnDeath();
+        }
+    }
+
+    private void SetHealthUI()
+    {
+        slider.value = CurrentHealth;
+        FillImage.color = Color.Lerp(ZeroHealthColor,FullHealthColor,CurrentHealth / StartingHealth);
+
+    }
+
+    private void OnDeath() 
+    { 
+        Dead = true; 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy")) 
+        {
+            CurrentHealth = CurrentHealth - 20;
+            SetHealthUI();
+            Debug.Log(CurrentHealth);
+
+        }
+    }
+}
