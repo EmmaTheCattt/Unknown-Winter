e5b0e7a0ca9dcfe13244e2c66a72ff2f35f42868
Viktor <truelsandthetruels@gmail.com>
2025-03-10 22:57:10 +0100

    interaction

---
+++ Assets/viktor folder/Interaction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Interaction : MoveCamera
+{
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Interactable")
+        {
+            Debug.Log("In View");
+        }
+    }
+    public void Update()
+    {
+        transform.position = CameraPosition.position + Cam_pos_height;
+    }
+}
+
