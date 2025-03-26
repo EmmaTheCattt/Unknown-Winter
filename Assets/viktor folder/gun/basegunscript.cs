96fc25327bcf6d9083d5b1a7444067e41f78c2e5
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-03-26 20:39:25 +0100

    Gun interval

--- Assets/viktor folder/gun/basegunscript.cs
+++ Assets/viktor folder/gun/basegunscript.cs
@@ -7,11 +7,20 @@ public class basegunscript : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    public float interval = 2;
+    public float time = 0;
+
+    private void Start()
+    {
+        time = interval;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        time += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Mouse0) && time >= interval)
         {
+            time = 0;
             var flarebullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             flarebullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
         }
