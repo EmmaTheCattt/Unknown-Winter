4ddea6d203b401a8abb6178e92c1482dde26dd2d
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-03-31 09:03:20 +0200

    Flare logic

--- Assets/Scripts/flarebullet.cs
+++ Assets/Scripts/flarebullet.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class flarebullet : MonoBehaviour
 {
+    public Rigidbody RB;
+    public Collider Col;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody>();
+        Col = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            BulletLand();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BulletLand();
+    }
+
+    private void BulletLand()
+    {
+        RB.velocity = new Vector3(0, 0, 0);
+        RB.useGravity = false;
+        Col.enabled = false;
     }
 }
