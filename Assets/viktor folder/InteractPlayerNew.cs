74f12474fb6cefa1946a8236e186baec9a8baf63
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-05-12 13:24:21 +0200

    update

--- Assets/viktor folder/InteractPlayerNew.cs
+++ Assets/viktor folder/InteractPlayerNew.cs
@@ -25,6 +25,9 @@ public class InteractNew : MonoBehaviour
     {
         ray = Camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
+        float length = (ray.direction - ray.origin).magnitude;
+        Debug.Log(length);
+
         if (Physics.Raycast(ray, out var hit) && Input.GetMouseButtonDown(1))
         {
             // When LMouse is clicked - this runs
